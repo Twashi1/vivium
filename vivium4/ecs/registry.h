cc8cb846761b1824c48306edeb1e9183b1e4301e
Twashi1 <thomas.zarri@gmail.com>
2024-07-14 20:53:23 +0100

    ECS stub

---
+++ vivium4/ecs/registry.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// Archetype based ECS using static compile-time type generators for ID generation
+// https://ajmmertens.medium.com/building-an-ecs-2-archetypes-and-vectorization-fe21690805f9
+// ECS back and forth series
+
+namespace Vivium {
+	struct Registry {
+		// Sparse-set maps entity ids to archetype directly?
+		// 
+	};
+}
