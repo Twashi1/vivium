dc1b7e088bb45aa7683813427ef54aeaa34ed801
Twashi1 <thomas.zarri@gmail.com>
2024-07-29 20:43:02 +0100

    Nicer masks for version/identifier

--- vivium4/ecs/defines.cpp
+++ vivium4/ecs/defines.cpp
@@ -1,6 +1,14 @@
 #include "defines.h"
 
 namespace Vivium {
+	uint32_t getVersion(Entity entity) {
+		return (entity & ECS_VERSION_MASK) >> ECS_VERSION_SHIFT;
+	}
+
+	uint32_t getIdentifier(Entity entity) {
+		return entity & ECS_ENTITY_MASK;
+	}
+
 	uint32_t TypeGenerator::createIdentifier()
 	{
 		static uint32_t value = 0;
--- vivium4/ecs/defines.h
+++ vivium4/ecs/defines.h
@@ -10,21 +10,21 @@ namespace Vivium {
 	constexpr uint32_t ECS_VERSION_MAX = 0xfff;
 	constexpr uint32_t ECS_ENTITY_DEAD = 0xffffffff;
 
+	constexpr uint32_t ECS_VERSION_MASK = 0xfff00000;
+	constexpr uint32_t ECS_VERSION_SHIFT = 20;
+	constexpr uint32_t ECS_ENTITY_MASK = 0x000fffff;
+
 	constexpr uint8_t ECS_COMPONENT_MAX = 0xff;
 
 	typedef uint32_t Entity;
 	typedef std::bitset<ECS_COMPONENT_MAX> Signature;
 
-	constexpr inline uint32_t getVersion(Entity entity) {
-		return (entity & 0xfff00000) >> 20;
-	}
+	uint32_t getVersion(Entity entity);
+	uint32_t getIdentifier(Entity entity);
 
-	constexpr inline uint32_t getIdentifier(Entity entity) {
-		return entity & ECS_ENTITY_MAX;
-	}
-
-	constexpr Entity nullEntity = getIdentifier(ECS_ENTITY_MAX);
-	constexpr Entity tombstoneEntity = ECS_ENTITY_MAX & 0xfff00000;
+	// TODO: use both
+	constexpr Entity nullEntity = ECS_ENTITY_MAX & ECS_ENTITY_MASK;
+	constexpr Entity tombstoneEntity = ECS_ENTITY_MAX & ECS_VERSION_MASK;
 
 	struct TypeGenerator {
 		static uint32_t createIdentifier();
