dcf1136fe646aecf3a1b78e183634a5412de3e1a
Twashi1 <33568076+Twashi1@users.noreply.github.com>
2025-08-07 15:56:18 +0100

    Documentation for batch and image load.

--- vivium4/graphics/batch.h
+++ vivium4/graphics/batch.h
@@ -23,12 +23,45 @@ namespace Vivium {
 		Ref<Buffer> vertexStaging, indexStaging, vertexDevice, indexDevice;
 	};
 
+	/*! \brief Submit data for a single element to the batch.
+	* 
+	* For a given element in the buffer layout, this function fills in the data for that element using the contiguous data passed.
+	* 
+	* \param batch The batch to submit to.
+	* \param elementIndex The index of the element in the buffer layout.
+	* \param data The data to submit for the element.
+	*/
 	void submitElementBatch(Batch& batch, uint64_t elementIndex, const std::span<const uint8_t> data);
+	/*! \brief Submit a float rectangle to a specific element in a batch.
+	* 
+	* \param elementIndex The index of the element in the buffer layout.
+	*/
 	void submitRectangleBatch(Batch& batch, uint64_t elementIndex, float left, float bottom, float right, float top);
+	/*! \brief End the current shape with the given number of vertices.
+	* 
+	* \param vertexCount The number of vertices that were submitted.
+	* \param indices The indices for those vertices of the shape
+	*/
 	void endShapeBatch(Batch& batch, uint64_t vertexCount, const std::span<const uint16_t> indices);
+	/*! \brief Finalise the batch and update the buffers with the new data.
+	* 
+	* The batch's vertex and index buffers are now ready for rendering/transfer.
+	*/
 	void endSubmissionBatch(Batch& batch, CommandContext& context, Engine& engine);
 
+	/*! \brief Get the vertex buffer of the batch
+	* 
+	* Requires that the batch submission was ended.
+	* 
+	* \return A reference to the buffer.
+	*/
 	Buffer const& vertexBufferBatch(Batch const& batch);
+	/*! \brief Get the index buffer of the batch
+	*
+	* Requires that the batch submission was ended.
+	*
+	* \return A reference to the buffer.
+	*/
 	Buffer const& indexBufferBatch(Batch const& batch);
 	// Returns index count of last endSubmission
 	uint32_t indexCountBatch(Batch const& batch);
--- vivium4/graphics/image_load.h
+++ vivium4/graphics/image_load.h
@@ -15,6 +15,20 @@ namespace Vivium {
 		TextureFormat format;
 	};
 
+	/*! \brief Load image from a file, given a texture format.
+	* 
+	* The loaded image will need to be dropped. Uses stbi, many image formats supported.
+	* 
+	* \param filename The file to load the image from.
+	* \param format The texture format of the image.
+	* 
+	* \return An image object.
+	*/
 	Image loadImage(char const* filename, TextureFormat format);
+	/*! \brief Free the image data on an image.
+	* Any copies of the image will also be freed by this.
+	* 
+	* \param image The image to free.
+	*/
 	void dropImage(Image& image);
 }
