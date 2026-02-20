ea9fbfaff3c0320e12e3ae232cf58d2a1a767004
Twashi1 <thomas.zarri@gmail.com>
2026-02-20 20:57:15 +0000

    Fleshed out most of the background, need collision resolution and rendering

--- verlet/state.cpp
+++ verlet/state.cpp
@@ -21,6 +21,137 @@ void updatePoint(Point& point, float dt) {
   point.acceleration = F32x2(0.0f);
 }
 
+Cell createCell() {
+  Cell cell;
+  cell.pointCount = 0;
+  cell.pointIndices = new uint16_t[MAX_CELL_OBJECTS];
+
+  return cell;
+}
+
+void dropCell(Cell& cell) {
+  if (cell.pointIndices == nullptr) return;
+  delete[] cell.pointIndices;
+}
+
+void swapRemoveFromCell(Cell& cell, uint16_t index) {
+  cell.pointIndices[index] = cell.pointIndices[--cell.pointCount];
+}
+
+World createWorld(F32x2 screenDim, uint16_t gridWidth, uint16_t gridHeight) {
+  World world;
+  world.gravity = F32x2(0.0f, -450.0f);
+  world.center = F32x2(400.0f, 300.0f);
+  world.dim = F32x2(0.0f);
+  world.radius = 200.0f;
+
+  world.gridHeight = gridHeight;
+  world.gridWidth = gridWidth;
+
+  world.size = 0;
+  world.gridDimensions = screenDim / F32x2(gridWidth, gridHeight);
+  world.igridDimensions = F32x2(1.0f) / world.gridDimensions;
+  world.resolutionCounter = 0;
+
+  world.points = std::vector<Point>(MAX_VERLET_OBJECTS);
+  world.cells = std::vector<Cell>(gridWidth * gridHeight);
+
+  for (uint64_t i = 0; i < gridWidth * gridHeight; i++) {
+    world.cells[i] = createCell();
+  }
+
+  return world;
+}
+
+void dropWorld(World& world) {
+  for (Cell& cell : world.cells) {
+    dropCell(cell);
+  }
+}
+
+void updateCells(World& world) {
+  for (uint32_t gy = 0; gy < world.gridHeight; gy++) {
+    for (uint32_t gx = 0; gx < world.gridWidth; gx++) {
+      Cell& cell = world.cells[gx + gy * world.gridWidth];
+
+      for (uint64_t i = 0; i < cell.pointCount; i++) {
+        uint16_t objIndex = cell.pointIndices[i];
+
+        Point& point = world.points[objIndex];
+
+        Vec2<uint16_t> gridPos =
+            F32x2::floor(F32x2(point.current) * world.igridDimensions);
+
+        if (gridPos.x == gx && gridPos.y == gy) {
+          continue;
+        }
+
+        // Remove from old cell
+        swapRemoveFromCell(cell, i);
+
+        // Add to new
+        Cell& targetCell = world.cells[gridPos.x + gridPos.y * world.gridWidth];
+        targetCell.pointIndices[targetCell.pointCount++] = objIndex;
+      }
+    }
+  }
+}
+
+void applyGravity(World& world) {
+  for (uint64_t i = 0; i < world.size; i++) {
+    world.points[i].acceleration += world.gravity;
+  }
+}
+
+void constraintSquare(World& world) {
+  float left = world.center.x - world.dim.x;
+  float right = world.center.x + world.dim.x;
+  float bot = world.center.y - world.dim.y;
+  float top = world.center.y + world.dim.y;
+
+  for (uint64_t i = 0; i < world.size; i++) {
+    Point& point = world.points[i];
+
+    if (point.current.x - point.radius < left) {
+      point.current.x = left + point.radius;
+    } else if (point.current.x + point.radius > right) {
+      point.current.x = right - point.radius;
+    }
+
+    if (point.current.y - point.radius < bot) {
+      point.current.y = bot + point.radius;
+    } else if (point.current.y + point.radius > top) {
+      point.current.y = top - point.radius;
+    }
+  }
+}
+
+void collisionResolution(World& world) {
+  // TODO: implementation
+}
+
+void updatePosition(World& world, float dt) {
+  for (uint64_t i = 0; i < world.size; i++) {
+    updatePoint(world.points[i], dt);
+  }
+}
+
+void update(World& world) {
+  const int physicsSubsteps = 4;
+  // 60fps rendering rate
+  const float timePerFrame = 0.016f;
+  const float substepDt = timePerFrame / physicsSubsteps;
+
+  updateCells(world);
+
+  for (int i = 0; i < physicsSubsteps; i++) {
+    applyGravity(world);
+    constraintSquare(world);
+    collisionResolution(world);
+    updatePosition(world, substepDt);
+  }
+}
+
 void _submit(State& state) {}
 
 void _setup(State& state) {}
--- verlet/state.h
+++ verlet/state.h
@@ -4,6 +4,9 @@
 
 using namespace Vivium;
 
+constexpr uint64_t MAX_VERLET_OBJECTS = 8000;
+constexpr uint64_t MAX_CELL_OBJECTS = 800;
+
 namespace Verlet {
 struct State {
   Engine engine;
@@ -21,8 +24,53 @@ struct Point {
   Color color;
 };
 
+struct Cell {
+  uint64_t pointCount;
+  uint16_t* pointIndices;
+};
+
+struct World {
+  F32x2 gravity;
+  F32x2 center;
+  F32x2 dim;
+  float radius;
+
+  uint64_t size;
+  std::vector<Point> points;
+  std::vector<Cell> cells;
+  uint32_t gridWidth;
+  uint32_t gridHeight;
+
+  F32x2 gridDimensions;
+  F32x2 igridDimensions;
+
+  uint64_t resolutionCounter;
+
+  Time::Timer timer;
+
+  // TODO: rendering information, maybe separate out
+};
+
 Point createPoint(F32x2 pos, float radius, Color color);
 void updatePoint(Point& point, float dt);
+
+Cell createCell();
+// NOTE: this is not the index of the point within the global point list, but
+// within the cell we have no other way to identify a point within the cell in
+// constant time
+void swapRemoveFromCell(Cell& cell, uint16_t index);
+void dropCell(Cell& cell);
+
+World createWorld();
+void dropWorld(World& world);
+
+void updateCells(World& world);
+void update(World& world);
+
+void applyGravity(World& world);
+void constraintSquare(World& world);
+void collisionResolution(World& world);
+void updatePosition(World& world, float dt);
 
 void init(State* state);
 void run(State* state);
